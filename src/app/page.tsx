5f35ec0035adb298e8883163caba800e0da0642f
Joao Souza <joaoosouza07@gmail.com>
2023-11-12 11:41:38 -0300

    :sparkles: feat: add styled components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import * as S from "./styles";
+
 export default function Home() {
-    return <div>Home</div>;
+    return (
+        <S.Container>
+            <p>Home</p>
+        </S.Container>
+    );
 }
